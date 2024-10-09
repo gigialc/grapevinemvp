cef26e29894929208ed8af2059e3a43c09746b4d
Ashley Garcia <ashley.garcia.official@gmail.com>
2024-10-09 17:05:03 -0400

    fix: api calls (still in progress)

--- grapevinemvp/app/add-project/page.js
+++ grapevinemvp/app/add-project/page.js
@@ -1,11 +1,6 @@
 'use client'
 
-import { Suspense } from 'react'
-import { useRouter, useSearchParams } from 'next/navigation';
-import { useSession } from "next-auth/react";
-import Image from 'next/image';
-import Navbar from '../components/Navbar';
-import { useState, useEffect } from 'react';
+import { Suspense } from 'react';
 import AddProjectContent from '../components/AddProjectContent';
 
 export default function AddProject() {
