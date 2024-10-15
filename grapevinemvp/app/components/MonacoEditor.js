e6fddea870ef5b17e20faf98ef596bb379ed5574
gigialcaraz <115032403+gigialc@users.noreply.github.com>
2024-10-15 08:18:54 -0400

    hi

--- grapevinemvp/app/components/MonacoEditor.js
+++ grapevinemvp/app/components/MonacoEditor.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useRef } from 'react';
-import * as monaco from 'monaco-editor';
+// import * as monaco from 'monaco-editor';
 
 const MonacoEditor = () => {
   const editorRef = useRef(null);
 
   useEffect(() => {
+    import('monaco-editor').then((monaco) => {
     const editor = monaco.editor.create(editorRef.current, {
       value: `function hello() {\n\tconsole.log("Hello, world!");\n}`,
       language: 'javascript',
@@ -15,6 +16,7 @@ const MonacoEditor = () => {
     return () => {
       editor.dispose(); // Clean up the editor when the component unmounts
     };
+  } );
   }, []);
 
   return <div ref={editorRef} style={{ height: '90vh', width: '100%' }}></div>;
