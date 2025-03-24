52d45df862e656ffe1dee14b1505b5c95dd15372
gigialcaraz <115032403+gigialc@users.noreply.github.com>
2025-03-23 20:36:45 -0400

    icon change

--- grapevinemvp/app/layout.js
+++ grapevinemvp/app/layout.js
@@ -5,6 +5,16 @@ import Navbar from './components/Navbar'
 // app/layout.js
 import { Providers } from "./providers";
 
+export const metadata = {
+  title: 'WXI',
+  description: 'Connect, collaborate, and create impact together',
+  icons: {
+    icon: '/images/default-avatar.png',
+    apple: '/images/default-avatar.png',
+    shortcut: '/images/default-avatar.png'
+  }
+};
+
 export default function RootLayout({ children }) {
   return (
     <html>
